c82b312ad0320ea38cdf5fde56051c3bba314fb0
Adam Kelsey <adam.kelsey113@gmail.com>
2016-08-24 17:41:49 -0400

    first two challenges finished

---
+++ answers.js
@@ -0,0 +1,8 @@
+// Challenge 1 print to console HELLO WORLD
+console.log('HELLO WORLD')
+
+// Challenge 2 Write a program that accepts one or more numbers as command-line arguments and prints the sum of those numbers to the console (stdout).
+var total = 0
+for (var i = 2; i < process.argv.length; i++)
+  total += Number(process.argv[i])
+console.log(total)

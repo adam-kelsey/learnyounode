36f1b9ce802041254b033d42582b4f1db6d868cc
Adam Kelsey <adam.kelsey113@gmail.com>
2016-08-25 19:01:40 -0400

    challenge 4 completed, better understanding of asynchronous

--- answers.js
+++ answers.js
@@ -1,11 +1,16 @@
 // Challenge 1 print to console HELLO WORLD
 console.log('HELLO WORLD')
 
+
+
 // Challenge 2 Write a program that accepts one or more numbers as command-line arguments and prints the sum of those numbers to the console (stdout).
 var total = 0
 for (var i = 2; i < process.argv.length; i++)
   total += Number(process.argv[i])
 console.log(total)
+
+
+
 
 // Challenge 3 Write a program that uses a single synchronous filesystem operation to read a file and print the number of newlines (\n) it contains to the console
 var fs = require('fs');
@@ -13,3 +18,20 @@ var buf = fs.readFileSync(process.argv[2]);
 var str = buf.toString();
 var lines = str.split('\n');
 console.log(lines.length - 1);
+
+
+
+
+
+// Challenge 4 Write a program that uses a single asynchronous filesystem operation to read a file and print the number of newlines it contains to the console
+var fs = require('fs');
+var file = process.argv[2];
+
+fs.readFile(file, 'utf8', function (err, data) {
+  var lines = data.split('\n').length - 1
+  console.log(lines)
+});
+
+
+
+

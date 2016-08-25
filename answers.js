c9183c54c30e6fdac39f161c1ca32766d5504406
Adam Kelsey <adam.kelsey113@gmail.com>
2016-08-25 18:29:30 -0400

    challenge 3 finished and understood

--- answers.js
+++ answers.js
@@ -6,3 +6,10 @@ var total = 0
 for (var i = 2; i < process.argv.length; i++)
   total += Number(process.argv[i])
 console.log(total)
+
+// Challenge 3 Write a program that uses a single synchronous filesystem operation to read a file and print the number of newlines (\n) it contains to the console
+var fs = require('fs');
+var buf = fs.readFileSync(process.argv[2]);
+var str = buf.toString();
+var lines = str.split('\n');
+console.log(lines.length - 1);

b38d34db83d41c8b6a43db924592f85cd0153ebf
Adam Kelsey <adam.kelsey113@gmail.com>
2016-08-31 16:15:18 -0400

    Finished challenge 5

--- answers.js
+++ answers.js
@@ -35,3 +35,17 @@ fs.readFile(file, 'utf8', function (err, data) {
 
 
 
+// Challenge 5 Create a program that prints a list of files in a given directory, filtered by the extension of the files.
+var fs = require('fs')
+var path = require('path')
+
+fs.readdir(process.argv[2], function (err, data) {
+  if (err) return console.error(err)
+  data.forEach(function (file) {
+    if (path.extname(file) === '.' + process.argv[3])
+      console.log(file)
+  })
+})
+
+
+
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var fs = require('fs');
-var file = process.argv[2];
+var fs = require('fs')
+var path = require('path')
 
-fs.readFile(file, 'utf8', function (err, data) {
-  var lines = data.split('\n').length - 1
-  console.log(lines)
-});
+fs.readdir(process.argv[2], function (err, data) {
+  if (err) return console.error(err)
+  data.forEach(function (file) {
+    if (path.extname(file) === '.' + process.argv[3])
+      console.log(file)
+  })
+})
